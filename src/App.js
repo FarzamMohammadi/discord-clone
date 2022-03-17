f20ced5c6fa1ddd5680e18d3ce29cf1b4caca96f
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2022-03-16 22:19:34 -0400

    Sidebar & top nav complete

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import Channelbar from './components/ChannelBar';
+// import ContentContainer from './components/ContentContainer';
+import TopNavigation from './components/TopNavigation';
+import SideBar from './components/SideBar';
+
+function App() {
+  return (
+    <div className='flex'>
+      <TopNavigation />
+      <SideBar />
+      {/* <Channelbar />
+      <ContentContainer /> */}
+    </div>
+  );
+}
+
+export default App;
